b464aa2f6fb32c2dfa37182302efe6870e5c65bf
alexbstl <alexbstl@gmail.com>
2013-09-27 00:16:07 -0500

    v1 of project

--- GameTheoryProject/src/game/Bird.java
+++ GameTheoryProject/src/game/Bird.java
@@ -2,10 +2,18 @@ package game;
 
 public class Bird {
 	private int strategy;
+	public boolean canBeAltered;
+	public int Score;
+	private boolean canplay;
+	public final int identifier;
 
-	public Bird(int type) {
+	public Bird(int type, int identifier) {
 		super();
+		this.identifier=identifier;
 		this.strategy = type;
+		this.canBeAltered=true;
+		Score=0;
+		this.setPlay(true);
 	}
 	public int getType() {
 		return strategy;
@@ -13,5 +21,12 @@ public class Bird {
 
 	public void setStrategy(int type) {
 		this.strategy = type;
+	}
+	public void setPlay(boolean play){
+		canplay=play;
+	}
+	public boolean getPlay(){
+		return canplay;
+
 	}
 }
--- GameTheoryProject/src/game/Game.java
+++ GameTheoryProject/src/game/Game.java
@@ -1,34 +1,85 @@
 package game;
 
-import java.util.Set;
 
-public class Game {
-	public Set<Bird> Birds;
-	public Set<Bird> Hawk;
-	public Set<Bird> Dove;
-	public double HawkScore;
-	public double DoveScore;
-	public double cost;
-	public double win;
-	public static void main(String args[]){
-
+public class Game extends Thread {
+	public Bird a;
+	public Bird b;
+	private Thread thread;
+	public int win;
+	public int cost;
+	public Game (Bird a, Bird b, int win,int cost){
+		this.a=a;
+		this.b=b;
+		this.win=win;
+		this.cost=cost;
 	}
-
-	public void game(Bird A, Bird B){
-		int TypeA=A.getType();
-		int TypeB=B.getType();
-		if(TypeA==1&&TypeB==1){
-			HawkScore+=win-2*cost;
-		}
-		if(TypeA+TypeB==1){
-			HawkScore+=win;
-		}
-		if(TypeA+TypeB==0){
-			DoveScore+=win;
-		}
+	public void rungame(){
+		System.out.println("win is worth " + win + " cost is worth "+ cost);
+		thread=new Thread(){
+			public void run(){
+				if(birdcheck()){
+					removebirds();
+					if(a.getPlay()&&b.getPlay()){
+						playGame();
+						System.out.println("Bird A is type "+a.getType()+" and has score: "+a.Score);
+						System.out.println("Bird B is type "+b.getType()+" and has score: "+b.Score);
+						a.setPlay(false);
+						b.setPlay(false);
+						System.out.println("Bird "+a.identifier + " played Bird " + b.identifier);
+					}			
+				}
+			}
+		};
+		thread.start();
 	}
-	public void evolve(double HawkScore, double DoveScore){
+	public Thread getThread() {
+		return thread;
+	}
+	public void setThread(Thread thread) {
+		this.thread = thread;
+	}
+	private boolean birdcheck(){
+		if(a.canBeAltered&&b.canBeAltered){
+			return true;
+		} else
+			try {
+				sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		return false;
+	}
+	private void removebirds(){
+		a.canBeAltered=false;
+		b.canBeAltered=false;
+	}
+	private void freebirds(){
+		a.canBeAltered=true;
+		b.canBeAltered=true;
+	}
+	private void playGame(){
+		if(a.getType()==1){
+			//System.out.println("a is a hawk");
+			if(b.getType()==0){
+				a.Score+=win;
+			}else if(b.getType()==1){
+				a.Score+=(win/2);
+				a.Score-=cost;
+				b.Score+=(win/2);
+				b.Score-=cost;
+			}
 
+		}else if(a.getType()==0){
+			//System.out.println("a is a peaceloving hippie");
+			if(b.getType()==0){
+				a.Score+=(win/2);
+				b.Score+=(win/2);
+			}else if(b.getType()==1){
+				b.Score+=win;
+			}
+
+		}
+		freebirds();
 	}
 
 }
